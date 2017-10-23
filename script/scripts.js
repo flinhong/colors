9d7e74a5def9ba1e188e39e52ead7938edb9e595
Frank Lin <flinhong@hotmail.com>
2017-10-23 21:42:43 +0800

    update backgroud animate

--- script/scripts.js
+++ script/scripts.js
@@ -1,30 +1,4 @@
 $( document ).ready(function() {
-  $("#preloader").fadeOut("slow", function() {
-    $(this).remove();
-  });
-
-  $('#colors .card, #details .card, #infors .card').fadeIn('slow');
-
-  $('#details .dd-c').peity('donut', {
-    fill: ["#0d5661", "#eee"],
-    innerRadius: 36,
-    radius: 40
-  });
-  $('#details .dd-m').peity('donut', {
-    fill: ["#cb1b45", "#eee"],
-    innerRadius: 36,
-    radius: 40
-  });
-  $('#details .dd-y').peity('donut', {
-    fill: ["#ffc408", "#eee"],
-    innerRadius: 36,
-    radius: 40
-  });
-  $('#details .dd-k').peity('donut', {
-    fill: ["#1c1c1c", "#eee"],
-    innerRadius: 36,
-    radius: 40
-  });
 
   function cmykCircles() {
     $('#colors .dd-c:not(.initialed)').peity('donut', {
@@ -48,7 +22,35 @@ $( document ).ready(function() {
       radius: 18
     });
   }
-  setTimeout(cmykCircles, 400);
+
+  setTimeout(cmykCircles, 500);
+  $.when( cmykCircles() ).done(function() {
+    $("#preloader").toggle( "clip" );
+    $("#preloader").remove();
+  }, 500);
+
+  $('#colors .card, #details .card, #infors .card').fadeIn('slow');
+
+  $('#details .dd-c').peity('donut', {
+    fill: ["#0d5661", "#eee"],
+    innerRadius: 36,
+    radius: 40
+  });
+  $('#details .dd-m').peity('donut', {
+    fill: ["#cb1b45", "#eee"],
+    innerRadius: 36,
+    radius: 40
+  });
+  $('#details .dd-y').peity('donut', {
+    fill: ["#ffc408", "#eee"],
+    innerRadius: 36,
+    radius: 40
+  });
+  $('#details .dd-k').peity('donut', {
+    fill: ["#1c1c1c", "#eee"],
+    innerRadius: 36,
+    radius: 40
+  });
 
   function hexToRgb(hex) { //https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
@@ -78,54 +80,11 @@ $( document ).ready(function() {
     var thisE = $(this);
     $(this).on('click', function() {
       $('#colors .change-color').removeClass('change-color');
-      changeColor(thisE);
-      // var name = $(this).find('.card-title').text();
-      // var enname = $(this).find('.en-name').text();
-      // var hex = $(this).find('.hex-value').text().toLowerCase();
-      // var cmyk_c = $(this).find('.dd-c').text();
-      // var cmyk_m = $(this).find('.dd-m').text();
-      // var cmyk_y = $(this).find('.dd-y').text();
-      // var cmyk_k = $(this).find('.dd-k').text();
-      // var rgb = hexToRgb(hex);
-      // var rgb_text = 'rgb(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ')';
-      // var luma = 0.2126 * rgb.r + 0.7152 * rgb.g + 0.0722 * rgb.b;
-      // if (luma<150) {
-      //   $('body').css('color', '#eee')
-      // } else {
-      //   $('body').css('color', '#1c1c1c')
-      // }
-      // if (luma>200) {
-      //   $('.main').css('background-color', 'rgba(0, 0, 0, 0.2)')
-      // } else {
-      //   $('.main').css('background-color', 'transparent')
-      // }
-
-      // $('#infors').animate({'opacity': 0}, 400, function () {
-      //     $(this).find('.card-title').hide(0).text(name).fadeIn('slow');
-      //     $(this).find('.en-name').text(enname).hide(0).fadeIn('slow');
-      //     $(this).find('.card-footer p:first').text(hex);
-      //     $(this).find('.card-footer p:last').text(rgb_text);
-      // }).animate({'opacity': 1}, 400);
-
-      // $('#details .dd-c').text(cmyk_c).change();
-      // $('#details .dd-c').prev().text('C: ' + cmyk_c.split('/', 1));
-      // $('#details .dd-m').text(cmyk_m).change();
-      // $('#details .dd-m').prev().text('M: ' + cmyk_m.split('/', 1));
-      // $('#details .dd-y').text(cmyk_y).change();
-      // $('#details .dd-y').prev().text('Y: ' + cmyk_y.split('/', 1));
-      // $('#details .dd-k').text(cmyk_k).change();
-      // $('#details .dd-k').prev().text('K: ' + cmyk_k.split('/', 1));
-
-      // $('#details .bg-r').css('height', rgb.r*100/255+'%');
-      // $('#details .bg-g').css('height', rgb.g*100/255+'%');
-      // $('#details .bg-b').css('height', rgb.b*100/255+'%');
-
-      // $(this).addClass('change-color');
-      // $('body').css('background-color', hex);
+      changeColor(thisE,1000);
     });
   });
 
-  function changeColor(element) {
+  function changeColor(element,animate) {
       var name = $(element).find('.card-title').text();
       var enname = $(element).find('.en-name').text();
       var hex = $(element).find('.hex-value').text().toLowerCase();
@@ -138,9 +97,9 @@ $( document ).ready(function() {
 
       var luma = 0.2126 * rgb.r + 0.7152 * rgb.g + 0.0722 * rgb.b;
       if (luma<150) {
-        $('body').css('color', '#eee')
+        $('body, footer p a').css('color', '#eee')
       } else {
-        $('body').css('color', '#1c1c1c')
+        $('body, footer p a').css('color', '#1c1c1c')
       }
       if (luma>200) {
         $('.main').css('background-color', 'rgba(0, 0, 0, 0.2)')
@@ -148,8 +107,8 @@ $( document ).ready(function() {
         $('.main').css('background-color', 'transparent')
       }
       
-      $('#infors').find('.card-title').hide(0).text(name).fadeIn('fast');
-      $('#infors').find('.en-name').text(enname).hide(0).fadeIn('fast');
+      $('#infors').find('.card-title').text(name).effect( "bounce", "slow" );
+      $('#infors').find('.en-name').text(enname);
       $('#infors').find('.card-footer p:first').text(hex);
       $('#infors').find('.card-footer p:last').text(rgb_text);
 
@@ -163,11 +122,17 @@ $( document ).ready(function() {
       $('#details .dd-k').prev().text('K: ' + cmyk_k.split('/', 1));
 
       $('#details .bg-r').css('height', rgb.r*100/255+'%');
+      $('#details .bg-r').text(rgb.r);
       $('#details .bg-g').css('height', rgb.g*100/255+'%');
+      $('#details .bg-g').text(rgb.g);
       $('#details .bg-b').css('height', rgb.b*100/255+'%');
+      $('#details .bg-b').text(rgb.b);
 
       $(element).addClass('change-color');
-      $('body').css('background-color', hex);
+      // $('body').css('background-color', hex);
+      $( "body" ).animate({
+        backgroundColor: hex
+      }, animate);
   }
 
   function copyToClipboard(element) {
@@ -192,5 +157,9 @@ $( document ).ready(function() {
   }
   var rad = getRandomInt(0, 250);
   var radE = $('#colors .col-2')[rad];
-  changeColor(radE);
+  changeColor(radE,0);
+  $('#colors').animate({
+      scrollTop: $(radE).offset().top - 15
+  }, 100);
+  
 });
